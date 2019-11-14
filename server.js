17bd4983ded5ca4ad529c38f26b4d8ac60db0893
Ignacio Bejarano <ignbejarano@gmail.com>
2019-11-13 22:35:00 -0300

    deleted express sessions

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const session = require('express-session');
 const auth = require('./middleware/auth');
 
 require('dotenv').config();
