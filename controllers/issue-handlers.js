fc7c600ebf28f52d605c69a09a86a7cb1b8ff0b8
Ignacio Bejarano <ignbejarano@gmail.com>
2019-11-20 10:21:19 -0300

    code splitting -> controllers in a new folder for issues!

---
+++ controllers/issue-handlers.js
@@ -0,0 +1,89 @@
+const Bug = require('../models/bug.model');
+
+
+exports.getAll = async (req, res) => {
+    try {
+        const issues = await Bug.find();
+        console.log('Getting all the issues!')
+        res.status(200).json({ issues, user: req.user })
+
+    } catch (error) {
+        console.log(' And error has ocurred! ', error.toString())
+        res.status(400).json('An error has ocurred getting all the Issues: ' + error.toString())
+    }
+}
+
+exports.getById = async (req, res) => {
+    try {
+        console.log('Issue request #', req.params.id);
+        const issue = await Bug.findById(req.params.id);
+        res.status(200).json({ issue, user: req.user });
+    } catch (error) {
+        console.log(' And error has ocurred! ', error.toString())
+        res.status(400).json('An error has ocurred getting this Issue: ' + error.toString())
+    }
+}
+
+exports.add = async (req, res) => {
+    try {
+        // TODO: Make this shorter
+        // 1. view req.body properties and save directly
+        const priority = req.body.priority;
+        const status = req.body.status;
+        const title = req.body.title;
+        const type = req.body.type;
+        const newIssue = new Bug({
+            priority,
+            status,
+            title,
+            type
+        })
+        const saveResponse = await newIssue.save();
+        console.log('New Issue Registered!');
+        res.status(200).json(saveResponse)
+
+    } catch (error) {
+        console.log(error.toString());
+        res.status(500).json(error.toString());
+    }
+}
+
+exports.update = async (req, res) => {
+    try {
+        console.log('Updating issue #', req.params.id);
+        const id = req.params.id
+        const issue = await Bug.findByIdAndUpdate(id, req.body);
+        console.log('Succesful issue update!')
+        res.status(200).json(issue);
+    } catch (error) {
+        console.log('Error ocurred during issue editing!', error.toString());
+        res.status(500).json(error.ToString());
+    }
+}
+
+exports.delete = async (req, res) => {
+    try {
+        const deleteResponse = await Bug.findByIdAndDelete(req.params.id);
+        console.log('Issue deleted!', deleteResponse);
+        res.status(200).json('Issue #' + req.params.id + ' deleted!')
+    } catch (error) {
+        console.log('Error deleting issue!')
+        res.status(500).json('Error deleting the issue!')
+    }
+}
+
+exports.addComment = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const author = req.user.username;
+        req.body.author = author
+        const issue = await Bug.findById(id)
+        issue.comments.push(req.body)
+        const saved = await issue.save()
+        console.log('new omment saved!', saved);
+        res.status(200).json('Comment added succesully!')
+    } catch (error) {
+        console.log('Error ocurrend when adding a comment!', error.ToString())
+        res.status(400).json(error);
+    }
+}
--- routes/bugs.js
+++ routes/bugs.js
@@ -1,91 +1,17 @@
 const router = require('express').Router();
 const Bug = require('../models/bug.model');
+const issueHandler = require('../controllers/issue-handlers');
 
+router.route('/').get(issueHandler.getAll)
 
-// SHOW BUGS
-router.route('/').get((req,res) => {
-    Bug.find()
-        .then(bugs => {
-            res.json({bugs, user: req.user})})
-        .catch(err => res.status(400).json('Error: '+err));
-})
+router.route('/:id').get(issueHandler.getById)
 
-// GET A SINGLE BUG INFO
-router.route('/:id').get((req,res) => {
-    console.log('Bug request #', req.params.id)
-    Bug.findById(req.params.id)
-        .then(bug => res.json({bug, user:req.user}))
-        .catch(err => res.status(400).json('Error: '+ err));
-})
+router.route('/add').post(issueHandler.add);
 
-// ADD A BUG
-router.route('/add').post( async (req, res) =>{
-    try{
-        const priority = req.body.priority;
-        const status = req.body.status;
-        const title = req.body.title;
-        const type = req.body.type;
-        const newBug = new Bug({
-            priority,
-            status,
-            title,
-            type
-        })
-    
-        const saveResponse =  await newBug.save();
-        console.log('New Bug Registered!');
-        res.status(200).json(saveResponse)
+router.route('/:id').put(issueHandler.update)
 
-    } catch(error){
-        console.log(error.toString())
-    }
-});
+router.route('/:id').delete(issueHandler.delete)
 
-// UPDATE BUG REGISTRY
-
-router.route('/update/:id').put(async (req, res) =>{
-    try{
-
-        console.log('Editing bug #', req.params.id);
-        const id = req.params.id
-        const bug = await Bug.findById(id);
-        console.log('Bug finded in db! #:', bug._id);
-        Object.assign(bug, req.body);
-        const resFromDb = await bug.save();
-        console.log('Succesful bug update!')
-        res.status(200).json(resFromDb);
-    } catch(error){
-        console.log('Error ocurred during bug editing!', error.toString());
-        res.status(500).json(error);
-    }
-})
-
-// DELETE BUG REGISTRY 
-router.route('/:id').delete( async (req,res) => {
-    const id = req.params.id;
-    try{
-        const res = await Bug.findByIdAndDelete(id);
-        res.status(200).json('Bug #' + id + ' deleted!')
-    } catch(error) {
-        res.status(500).json('Error deleting the bug!')
-    }
-})
-
-// ADD A COMMENT TO A BUG
-router.route('/add-comment/:id').put( async (req, res) =>{
-    const id = req.params.id;
-    const author = req.user.username;
-    req.body.author = author
-    try{
-        const bug = await Bug.findById(id)
-        bug.comments.push(req.body)
-        const saved = await bug.save()
-        console.log(saved);
-        res.status(200).json(saved)
-    } catch(error){
-        console.log('Error', error)
-        res.status(400).json(error)
-    }
-})
+router.route('/add-comment/:id').put(issueHandler.addComment)
 
 module.exports = router;
