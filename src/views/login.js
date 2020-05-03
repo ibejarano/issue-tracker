f0fc894de5ff62979441ddff7b532cc0e1c09de0
Ignacio Bejarano <ignbejarano@gmail.com>
2020-05-03 17:12:52 -0300

    login page refactor

--- src/views/login.js
+++ src/views/login.js
@@ -1,85 +1,46 @@
 import React, { useState } from "react";
-import {useHistory} from 'react-router-dom'
+import { useHistory } from "react-router-dom";
 import { authenticationService } from "../handlers/authentication";
 
 // Material ui components
 import Avatar from "@material-ui/core/Avatar";
-import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
-import TextField from "@material-ui/core/TextField";
-import Link from "@material-ui/core/Link";
-import Grid from "@material-ui/core/Grid";
 import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
+import Form from "../components/commons/Form";
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   "@global": {
     body: {
-      backgroundColor: theme.palette.common.white
-    }
+      backgroundColor: theme.palette.common.white,
+    },
   },
   paper: {
     marginTop: theme.spacing(8),
     display: "flex",
     flexDirection: "column",
-    alignItems: "center"
+    alignItems: "center",
   },
   avatar: {
     margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main
+    backgroundColor: theme.palette.secondary.main,
   },
-  form: {
-    width: "100%", // Fix IE 11 issue.
-    marginTop: theme.spacing(1)
-  },
-  submit: {
-    margin: theme.spacing(1, 0, 2)
-  },
-  errorText:{
-    color: 'red',
-    backgroundColor: 'pink',
-    border: '1px solid',
-    padding: '3px'
-  }
 }));
 
 export default function LoginPage(props) {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [badLogin, setBadLogin] = useState(false);
   const classes = useStyles();
-  const history = useHistory();
-  const handleEmail = e => {
-    setEmail(e.target.value);
-  };
+  const [error, setError] = useState(null);
 
-  const handlePassword = e => {
-    setPassword(e.target.value);
-  };
-
-  const onSubmit = async e => {
-    e.preventDefault();
-    const params = { email, password };
-    try {
-      const user = await authenticationService.login(
-        params.email,
-        params.password
-      );
-      props.setUser(user)
-      history.push('/user')
-    } catch (error) {
-      console.log(error.toString());
-      setPassword("");
-      setBadLogin(true)
-      history.push("/");
+  const handleSubmit = async (params) => {
+    const { data, error } = await authenticationService.login(params);
+    if (error) {
+      setError(error);
+    } else {
+      localStorage.setItem("issue-tracker-user", JSON.stringify(data));
     }
   };
-
-  if (authenticationService.currentUserValue ){
-    window.location = '/user'
-  }
 
   return (
     <Container component="main" maxWidth="xs">
@@ -91,56 +52,7 @@ export default function LoginPage(props) {
         <Typography component="h1" variant="h5">
           Log in
         </Typography>
-        <form className={classes.form} noValidate onSubmit={onSubmit}>
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="email"
-            label="Email Address"
-            name="email"
-            value={email}
-            onChange={handleEmail}
-            autoFocus
-          />
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
-            value={password}
-            onChange={handlePassword}
-          />
-          {
-            badLogin && <p className={classes.errorText}>Invalid user or password. Try again.</p>
-          }
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-          >
-            Log In
-          </Button>
-          <Grid container>
-            <Grid item xs>
-              <Link href="#" variant="body2">
-                Forgot password? (not implemented)
-              </Link>
-            </Grid>
-            <Grid item>
-              <Link href="/signup" variant="body2">
-                {"Don't have an account? Sign Up"}
-              </Link>
-            </Grid>
-          </Grid>
-        </form>
+        <Form handleSubmit={handleSubmit} error={error} />
       </div>
     </Container>
   );
