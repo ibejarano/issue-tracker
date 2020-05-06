f80e1b6bb01c9fce0b3d46ef1328a5b132458b7c
Ignacio Bejarano <ignbejarano@gmail.com>
2020-05-06 07:51:10 -0300

    add query parameters

--- controllers/issue-handlers.js
+++ controllers/issue-handlers.js
@@ -1,11 +1,14 @@
-const Bug = require('../models/bug.model');
+const Bug = require("../models/bug.model");
 
 exports.getAll = async (req, res) => {
   try {
-    const issues = await Bug.find({status: {$ne: 'Cerrado'}})
-      .sort({updatedAt: -1})
-      .populate('assignee');
-    res.status(200).json({issues, user: req.user});
+    const options = {
+      skip: 0,
+    };
+    const issues = await Bug.find({ status: { $ne: "Cerrado" } }, options)
+      .sort({ updatedAt: -1 })
+      .populate("assignee");
+    res.status(200).json({ issues });
   } catch (error) {
     console.log(error);
     res.status(400).json(error);
@@ -14,10 +17,20 @@ exports.getAll = async (req, res) => {
 
 exports.getArchived = async (req, res) => {
   try {
-    const issues = await Bug.find({status: 'Cerrado'})
-      .sort({updatedAt: -1})
-      .populate('assignee');
-    res.status(200).json({issues, user: req.user});
+    const { query } = req;
+    const { rows, page } = query;
+    if (!rows || !page) {
+      throw new Error("No se especifico parametros del query");
+    }
+    const totalCount = await Bug.countDocuments({ status: "Cerrado" });
+    const N = rows >= totalCount ? 1 : Math.floor(totalCount / rows) + 1;
+
+    const issues = await Bug.find({ status: "Cerrado" })
+      .skip(parseInt(page * rows))
+      .limit(parseInt(rows))
+      .sort({ updatedAt: -1 })
+      .populate("assignee");
+    res.status(200).json({ issues, totalCount: N, page: parseInt(page) });
   } catch (error) {
     console.log(error);
     res.status(400).json(error);
@@ -27,12 +40,12 @@ exports.getArchived = async (req, res) => {
 exports.getById = async (req, res) => {
   try {
     const issue = await Bug.findById(req.params.id)
-      .populate('assignee')
+      .populate("assignee")
       .populate({
-        path: 'comments.updateStatus.assignee',
+        path: "comments.updateStatus.assignee",
       })
       .exec();
-    res.status(200).json({issue, user: req.user});
+    res.status(200).json({ issue, user: req.user });
   } catch (error) {
     res.status(400).json(error.toString());
   }
@@ -40,7 +53,7 @@ exports.getById = async (req, res) => {
 
 exports.add = async (req, res, next) => {
   try {
-    const {priority, status, title, type} = req.body;
+    const { priority, status, title, type } = req.body;
     const newIssue = new Bug({
       priority,
       status,
@@ -57,10 +70,10 @@ exports.add = async (req, res, next) => {
 
 exports.update = async (req, res, next) => {
   try {
-    const {id} = req.params;
+    const { id } = req.params;
     const issue = await Bug.findByIdAndUpdate(id, req.body);
     issue.comments.push({
-      text: 'Estado actualizado',
+      text: "Estado actualizado",
       author: req.user.username,
       updateStatus: req.body,
     });
@@ -75,15 +88,15 @@ exports.update = async (req, res, next) => {
 exports.delete = async (req, res) => {
   try {
     await Bug.findByIdAndDelete(req.params.id);
-    res.status(200).json('Issue eliminado');
+    res.status(200).json("Issue eliminado");
   } catch (error) {
-    res.status(400).json('Error eliminando issue');
+    res.status(400).json("Error eliminando issue");
   }
 };
 
 exports.addComment = async (req, res, next) => {
   try {
-    const {id} = req.params;
+    const { id } = req.params;
     const author = req.user.username;
     req.body.author = author;
     const issue = await Bug.findById(id);
