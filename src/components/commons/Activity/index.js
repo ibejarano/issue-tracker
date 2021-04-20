ee2d8dcea64fe7ccf3a76662a74060ab43f035eb
Ignacio Bejarano <ignbejarano@gmail.com>
2021-04-20 10:01:04 -0300

    style userinfocard and activities card

--- src/components/commons/Activity/index.js
+++ src/components/commons/Activity/index.js
@@ -5,13 +5,15 @@ import clsx from "clsx";
 
 import Paper from "@material-ui/core/Paper";
 import LoadingCicle from "../../loading";
+import { Typography } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
   paper: {
-    padding: theme.spacing(2),
+    padding: theme.spacing(4),
     display: "flex",
     overflow: "auto",
     flexDirection: "column",
+    justifyContent: "flex-start"
   },
   fixedHeight: {
     height: 240,
@@ -22,18 +24,16 @@ export default function Activity({ user }) {
   const classes = useStyles();
   const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
 
+  if (!user) {
+    return <LoadingCicle />;
+  }
+
   return (
     <Paper className={fixedHeightPaper}>
-      <h2>Actividad</h2>
-      {user.username === "" ? (
-        <LoadingCicle />
-      ) : (
-        <ul>
-          {user.activities.map((activity, idx) => {
-            return <li key={idx}>{activity}</li>;
-          })}
-        </ul>
-      )}
+      <Typography variant="h6">Actividad</Typography>
+      {user.activities.map((activity, idx) => {
+        return <Typography key={idx}>{activity}</Typography>;
+      })}
     </Paper>
   );
 }
--- src/components/commons/UserInfoCard/index.js
+++ src/components/commons/UserInfoCard/index.js
@@ -1,19 +1,18 @@
 import React from "react";
 
+import { Paper, Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import clsx from "clsx";
 
-import Paper from "@material-ui/core/Paper";
-import Typography from "@material-ui/core/Typography";
 import LoadingCicle from "../../loading";
 import ChangePasswordDialog from "../../user/change-password";
 
 const useStyles = makeStyles((theme) => ({
   paper: {
-    padding: theme.spacing(2),
+    padding: theme.spacing(4),
     display: "flex",
-    overflow: "auto",
     flexDirection: "column",
+    justifyContent: "space-between"
   },
   fixedHeight: {
     height: 240,
@@ -24,17 +23,15 @@ export default function UserInfoCard({ user }) {
   const classes = useStyles();
   const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
 
+  if (!user) {
+    return <LoadingCicle />;
+  }
+
   return (
     <Paper className={fixedHeightPaper}>
-      {user.username === "" ? (
-        <LoadingCicle />
-      ) : (
-        <React.Fragment>
-          <Typography>Informacion del usuario</Typography>
-          <Typography>Nombre de usuario: {user.username}</Typography>
-          <Typography>E-mail: {user.email}</Typography>
-        </React.Fragment>
-      )}
+      <Typography variant="h6" >Informacion del usuario</Typography>
+      <Typography>Nombre de usuario: {user.username}</Typography>
+      <Typography>E-mail: {user.email}</Typography>
       <ChangePasswordDialog />
     </Paper>
   );
