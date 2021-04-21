e4752e5f536a1afc88e0b8ca049fadb28c76974a
Ignacio Bejarano <ignbejarano@gmail.com>
2021-04-21 09:11:22 -0300

    added drawer and open button

--- src/views/issue-list.js
+++ src/views/issue-list.js
@@ -1,8 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button, Drawer } from "@material-ui/core";
 
 import CustomMaterialTable from "../components/commons/MaterialTable";
 
 export default function IssueList({ isAdmin, setTitle }) {
+  const [openDrawer, setOpenDrawer] = useState(false);
+
   setTitle("Lista de Issues activos");
-  return <CustomMaterialTable isAdmin={isAdmin} />;
+
+  const handleAddIssue = () => {
+    setOpenDrawer(true);
+  };
+
+  const toggleDrawer = (open) => (event) => {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
+
+    setOpenDrawer(open);
+  };
+
+  return (
+    <>
+      <CustomMaterialTable isAdmin={isAdmin} />
+      <React.Fragment>
+        <Button onClick={handleAddIssue}>Open</Button>
+        <Drawer anchor="right" open={openDrawer} onClose={toggleDrawer(false)}>
+          Hola mundo!
+        </Drawer>
+      </React.Fragment>
+    </>
+  );
 }
